7c8e17a54d0be5770e59a9bc5e77f73060350262
Kris <krisgodom@gmail.com>
2020-11-16 23:35:28 -0400

    modify Arrays

--- src/Lesson2/Arrays.java
+++ src/Lesson2/Arrays.java
@@ -2,28 +2,20 @@ package Lesson2;
 
 public class Arrays {
     public static void main(String[] args) {
-        String usernamePassword = "Kris|qwerty";
-        int index = usernamePassword.indexOf('|');
-        String name = usernamePassword.substring(0, index);
-        String password = usernamePassword.substring(index + 1);
-        String inputPassword;
-        String inputName;
+        String usernamePassword = "Alex|qwertY1";
+        String[] dividedNamePassword = usernamePassword.split("\\|");
 
-if (args[0].contains("password")){
-    inputPassword = args[0].substring(args[0].indexOf("=")+1);
-    inputName = args[1].substring(args[1].indexOf("=")+1);
-}
-else if (args[0].contains("name")) {
-             inputName = args[0].substring(args[0].indexOf("=")+1);
-            inputPassword = args[1].substring(args[1].indexOf("=")+1);
-}
+        if (args[0].contains("password")){
+            String[] password = args[0].split("=");
+            String[] name = args[1].split("=");
+        }
+        else if (args[0].contains("name")){
+            String[] password = args[1].split("=");
+            String[] name = args[0].split("=");
+        }
 
-if (name.equals(inputName) && password.equals(inputPassword)){
-    System.out.println("Login successful");
-}
-else {
-    System.out.println("Login failed");
-}
-
+        if (password[1].equals(dividedNamePassword[1])){
+            System.out.println("Login successful");
+        }
     }
 }
--- src/Lesson2/Users.java
+++ src/Lesson2/Users.java
@@ -1,62 +1,45 @@
 package Lesson2;
 
 public enum Users {
-    ALEX("Alex","qwertY1", "Administrator"), MARK("Mark","ewq", "CustomerService"), LESYA("Lesya","qazS", "Analyst"), PETRO("Petro","1234", "Customer");
-    private String name;
-    private String password;
-    private String group;
+    ADMIN("all the permissions"), CUSTOMERSERVICE("specific permission"), ANALYST("view and buy permissions"), CUSTOMER("reports permissions");
+    private String permission;
 
-    Users(String name, String password, String group){
-        this.name = name;
-        this.password = password;
-        this.group = group;
+    Users(String permission){
+        this.permission = permission;
     }
-    public String getName(){
-        return name;
-    }
-    public String getPassword(){
-        return password;
-    }
-    public String getGroup(){
-        return group;
+
+    public String getPermission(){
+        return permission;
     }
 }
- class Test{
+class Test {
     public static void main(String[] args) {
-        Users alex = Users.valueOf("ALEX");
-        String alexName = alex.getName();
-        String alexPassword = alex.getPassword();
-        String alexGroup = alex.getGroup();
+String admin = "Alex|qwertY1|Admin";
+String customerService = "Mark|ewq|CustomerService";
+String analyst = "Lesya|qazS|Analyst";
+String customer = "Petro|1234|Customer";
 
-        Users mark = Users.valueOf("MARK");
-        String markName = mark.getName();
-        String markPassword = mark.getPassword();
-        String markGroup = mark.getGroup();
+String[] adminCredentials = admin.split("\\|");
+String[] customerServiceCredentials = customerService.split("\\|");
+String[] analystCredentials = analyst.split("\\|");
+String[] customerCredentials = customer.split("\\|");
 
-        Users lesya = Users.valueOf("LESYA");
-        String lesyaName = lesya.getName();
-        String lesyaPassword = lesya.getPassword();
-        String lesyaGroup = lesya.getGroup();
+if (adminCredentials[0].equals(args[0]) && adminCredentials[1].equals(args[1])){
+    System.out.println("Welcome, " + args[0] + "! Your group is " + Users.ADMIN + " and you have " + Users.ADMIN.getPermission());
+}
+   else if (customerServiceCredentials[0].equals(args[0]) && customerServiceCredentials[1].equals(args[1])){
+            System.out.println("Welcome, " + args[0] + "! Your group is " + Users.CUSTOMERSERVICE + " and you have " + Users.CUSTOMERSERVICE.getPermission());
+        }
+else if (analystCredentials[0].equals(args[0]) && analystCredentials[1].equals(args[1])){
+    System.out.println("Welcome, " + args[0] + "! Your group is " + Users.ANALYST + " and you have " + Users.ANALYST.getPermission());
+}
+else if (customerCredentials[0].equals(args[0]) && customerCredentials[1].equals(args[1])){
+    System.out.println("Welcome, " + args[0] + "! Your group is " + Users.CUSTOMER + " and you have " + Users.CUSTOMER.getPermission());
+}
+else {
+    System.out.println("Incorrect username/password");
+}
 
-        Users petro = Users.valueOf("PETRO");
-        String petroName = petro.getName();
-        String petroPassword = petro.getPassword();
-        String petroGroup = petro.getGroup();
-
-        if (alexName == args[0] && alexPassword == args[1] && alexGroup == args[2]){
-            System.out.println("Welcome, " + args[0] + "! Your group is " + args[2] + " and you have all the permissions");
-        }
-        else if (markName == args[0] && markPassword == args[1] && markGroup == args[2]){
-            System.out.println("Welcome, " + args[0] + "! Your group is " + args[2] + " and you have specific permission");
-        }
-        else if (lesyaName == args[0] && lesyaPassword == args[1] && lesyaGroup == args[2]){
-            System.out.println("Welcome, " + args[0] + "! Your group is " + args[2] + " and you have view and buy permissions");
-        }
-        else if (petroName == args[0] && petroPassword == args[1] && petroGroup == args[2]){
-            System.out.println("Welcome, " + args[0] + "! Your group is " + args[2] + " and you have reports permissions");
-        }
-        else {
-            System.out.println("Incorrect credentials!");
-        }
     }
+
 }
