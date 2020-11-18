a5d0c309017c6a162cfa82cda3c1b7d46fd08447
Kris <krisgodom@gmail.com>
2020-11-17 21:46:29 -0400

    final version

--- src/Lesson2/Arrays.java
+++ src/Lesson2/Arrays.java
@@ -4,18 +4,24 @@ public class Arrays {
     public static void main(String[] args) {
         String usernamePassword = "Alex|qwertY1";
         String[] dividedNamePassword = usernamePassword.split("\\|");
+        String arg1 [] = args[0].split("=");
+        String arg2 []= args[1].split("=");
 
         if (args[0].contains("password")){
-            String[] password = args[0].split("=");
-            String[] name = args[1].split("=");
+            if (arg1[1].equals(dividedNamePassword[1]) && arg2[1].equals(dividedNamePassword[0])){
+                System.out.println("Login successful");
+            }
+            else {
+                System.out.println("Login failed");
+            }
         }
         else if (args[0].contains("name")){
-            String[] password = args[1].split("=");
-            String[] name = args[0].split("=");
-        }
-
-        if (password[1].equals(dividedNamePassword[1])){
-            System.out.println("Login successful");
+            if (arg1[1].equals(dividedNamePassword[0]) && arg2[1].equals(dividedNamePassword[1])){
+                System.out.println("Login successful");
+            }
+            else {
+                System.out.println("Login failed");
+            }
         }
     }
 }
