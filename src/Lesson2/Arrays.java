40c6c154ec8d932d29564a458b30b00c58426100
Kris <krisgodom@gmail.com>
2020-11-19 14:12:24 -0400

    modified Users class

--- src/Lesson2/Arrays.java
+++ src/Lesson2/Arrays.java
@@ -4,22 +4,19 @@ public class Arrays {
     public static void main(String[] args) {
         String usernamePassword = "Alex|qwertY1";
         String[] dividedNamePassword = usernamePassword.split("\\|");
-        String arg1 [] = args[0].split("=");
-        String arg2 []= args[1].split("=");
+        String arg1[] = args[0].split("=");
+        String arg2[] = args[1].split("=");
 
-        if (args[0].contains("password")){
-            if (arg1[1].equals(dividedNamePassword[1]) && arg2[1].equals(dividedNamePassword[0])){
+        if (args[0].contains("password")) {
+            if (arg1[1].equals(dividedNamePassword[1]) && arg2[1].equals(dividedNamePassword[0])) {
                 System.out.println("Login successful");
-            }
-            else {
+            } else {
                 System.out.println("Login failed");
             }
-        }
-        else if (args[0].contains("name")){
-            if (arg1[1].equals(dividedNamePassword[0]) && arg2[1].equals(dividedNamePassword[1])){
+        } else if (args[0].contains("name")) {
+            if (arg1[1].equals(dividedNamePassword[0]) && arg2[1].equals(dividedNamePassword[1])) {
                 System.out.println("Login successful");
-            }
-            else {
+            } else {
                 System.out.println("Login failed");
             }
         }
--- src/Lesson2/Users.java
+++ src/Lesson2/Users.java
@@ -1,44 +1,41 @@
 package Lesson2;
 
 public enum Users {
-    ADMIN("all the permissions"), CUSTOMERSERVICE("specific permission"), ANALYST("view and buy permissions"), CUSTOMER("reports permissions");
+    ADMIN("all the permissions"), CUSTOMER_SERVICE("specific permission"), ANALYST("view and buy permissions"), CUSTOMER("reports permissions");
     private String permission;
 
-    Users(String permission){
+    Users(String permission) {
         this.permission = permission;
     }
 
-    public String getPermission(){
+    public String getPermission() {
         return permission;
     }
 }
+
 class Test {
     public static void main(String[] args) {
-String admin = "Alex|qwertY1|Admin";
-String customerService = "Mark|ewq|CustomerService";
-String analyst = "Lesya|qazS|Analyst";
-String customer = "Petro|1234|Customer";
+        String admin = "Alex|qwertY1|Admin";
+        String customerService = "Mark|ewq|CustomerService";
+        String analyst = "Lesya|qazS|Analyst";
+        String customer = "Petro|1234|Customer";
 
-String[] adminCredentials = admin.split("\\|");
-String[] customerServiceCredentials = customerService.split("\\|");
-String[] analystCredentials = analyst.split("\\|");
-String[] customerCredentials = customer.split("\\|");
+        String[] adminCredentials = admin.split("\\|");
+        String[] customerServiceCredentials = customerService.split("\\|");
+        String[] analystCredentials = analyst.split("\\|");
+        String[] customerCredentials = customer.split("\\|");
 
-if (adminCredentials[0].equals(args[0]) && adminCredentials[1].equals(args[1])){
-    System.out.println("Welcome, " + args[0] + "! Your group is " + Users.ADMIN + " and you have " + Users.ADMIN.getPermission());
-}
-   else if (customerServiceCredentials[0].equals(args[0]) && customerServiceCredentials[1].equals(args[1])){
-            System.out.println("Welcome, " + args[0] + "! Your group is " + Users.CUSTOMERSERVICE + " and you have " + Users.CUSTOMERSERVICE.getPermission());
+        if (adminCredentials[0].equals(args[0]) && adminCredentials[1].equals(args[1])) {
+            System.out.println("Welcome, " + args[0] + "! Your group is " + Users.ADMIN + " and you have " + Users.ADMIN.getPermission());
+        } else if (customerServiceCredentials[0].equals(args[0]) && customerServiceCredentials[1].equals(args[1])) {
+            System.out.println("Welcome, " + args[0] + "! Your group is " + Users.CUSTOMER_SERVICE + " and you have " + Users.CUSTOMER_SERVICE.getPermission());
+        } else if (analystCredentials[0].equals(args[0]) && analystCredentials[1].equals(args[1])) {
+            System.out.println("Welcome, " + args[0] + "! Your group is " + Users.ANALYST + " and you have " + Users.ANALYST.getPermission());
+        } else if (customerCredentials[0].equals(args[0]) && customerCredentials[1].equals(args[1])) {
+            System.out.println("Welcome, " + args[0] + "! Your group is " + Users.CUSTOMER + " and you have " + Users.CUSTOMER.getPermission());
+        } else {
+            System.out.println("Incorrect username/password");
         }
-else if (analystCredentials[0].equals(args[0]) && analystCredentials[1].equals(args[1])){
-    System.out.println("Welcome, " + args[0] + "! Your group is " + Users.ANALYST + " and you have " + Users.ANALYST.getPermission());
-}
-else if (customerCredentials[0].equals(args[0]) && customerCredentials[1].equals(args[1])){
-    System.out.println("Welcome, " + args[0] + "! Your group is " + Users.CUSTOMER + " and you have " + Users.CUSTOMER.getPermission());
-}
-else {
-    System.out.println("Incorrect username/password");
-}
 
     }
 
